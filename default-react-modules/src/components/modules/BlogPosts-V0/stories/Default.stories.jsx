ef65bab77aca6c8c06784b7bb7b32f82a940cb32
Brandon Van Dyck <bvandyck@hubspot.com>
2024-02-16 12:11:51 -0500

    Remove development folders

--- default-react-modules/src/components/modules/BlogPosts-V0/stories/Default.stories.jsx
+++
@@ -1,9 +0,0 @@
-import { Component, fields } from '../index.jsx';
-import { moduleStory } from '@hubspot/cms-dev-server/storybook';
-
-export default {
-  title: 'BlogPosts/Default',
-  component: Component,
-};
-
-export const Default = moduleStory(Component, fields);

c9b70dfbeaa4308350480ae1dfc1cddd6283db71
mshannon-hs <117389313+mshannon-hs@users.noreply.github.com>
2024-02-21 13:21:33 -0500

    Update config.ts

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -59,7 +59,7 @@ export default defineConfig({
                     link: 'js-modules#module-fields',
                   },
                   { text: 'GraphQL', link: 'js-modules#graphql' },
-                  { text: 'hublDataTemplate', link: 'js-modules#hublDataTemplate' },
+                  { text: 'hublDataTemplate', link: 'js-modules#hubldatatemplate' },
                 ],
               },
               { text: 'Islands', link: 'islands' },

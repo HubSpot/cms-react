ab68ea57feae43cb71fdce418fe1b907ee7ee44e
Jon Miller <jonmiller@hubspot.com>
2024-05-30 16:31:19 -0500

    add setup files

---
+++ examples/routing/routing-project/routing-app/components/islands/App.tsx
@@ -0,0 +1,71 @@
+import { Routes, Route, BrowserRouter, Link } from 'react-router-dom';
+import { StaticRouter } from 'react-router-dom/server';
+
+import {
+  useIsServerRender,
+  usePageUrl,
+  useBasePath,
+  logInfo
+} from '@hubspot/cms-components';
+
+function Home() {
+  return (
+    <main>
+      <h1>Home</h1>
+      <p>
+        <Link to={'/about'}>About</Link>
+      </p>
+    </main>
+  );
+}
+
+
+function About() {
+  return (
+    <main>
+      <h1>About</h1>
+      <p>
+        <Link to={'/home'}>Home</Link>
+      </p>
+    </main>
+  );
+}
+
+const AppRoutes = () => {
+  return (
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/home" element={<Home />} />
+      <Route path="/about" element={<About />} />
+    </Routes>
+  );
+};
+
+const App = () => {
+  const isServerRender = useIsServerRender();
+  const pageUrl = usePageUrl();
+  const basePath = useBasePath();
+
+  logInfo(basePath);
+  logInfo(pageUrl);
+
+  let app;
+
+  if (isServerRender) {
+    app = (
+      <StaticRouter basename={basePath} location={pageUrl.pathname}>
+        <AppRoutes />
+      </StaticRouter>
+    );
+  } else {
+    app = (
+      <BrowserRouter basename={basePath}>
+        <AppRoutes />
+      </BrowserRouter>
+    );
+  }
+
+  return app;
+};
+
+export default App;

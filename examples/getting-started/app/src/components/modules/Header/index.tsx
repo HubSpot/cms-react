c15fb18efdf4fab1ccd6951798431ec036b0908f
Jon Miller <jonmiller@hubspot.com>
2024-06-10 09:52:59 -0500

    header menu styles

--- examples/getting-started/app/src/components/modules/Header/index.tsx
+++ examples/getting-started/app/src/components/modules/Header/index.tsx
@@ -11,7 +11,7 @@ export function Component({ fieldValues }) {
   return (
     <header className={headerStyles.wrapper}>
       <nav>
-        <img src={fieldValues.logo.src} alt="Logo" />
+        <img src={fieldValues.logo.src} alt="Logo" width={100} />
         {/* <Menu fieldPath="menu" /> */}
         <div>
           <span
@@ -158,7 +158,12 @@ const DEFAULT_MENU_ID = '53522818668';
 
 export const fields = (
   <ModuleFields>
-    <ImageField name="logo" label="Logo" default={{ src: logo }} />
+    <ImageField
+      name="logo"
+      label="Logo"
+      default={{ src: logo }}
+      resizable={true}
+    />
     <MenuField name="menu" label="Menu" default={DEFAULT_MENU_ID} />
   </ModuleFields>
 );

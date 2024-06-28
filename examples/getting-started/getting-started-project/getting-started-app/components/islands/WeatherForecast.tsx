a7868555e19e0922864bf79a21482258718dfdaa
Jon Miller <jonmiller@hubspot.com>
2024-06-28 13:18:52 -0500

    refactor weather example to use api that does not require apikey

--- examples/getting-started/getting-started-project/getting-started-app/components/islands/WeatherForecast.tsx
+++ examples/getting-started/getting-started-project/getting-started-app/components/islands/WeatherForecast.tsx
@@ -1,60 +1,42 @@
 import { useEffect, useState } from 'react';
 import weatherStyles from '../../styles/weather.module.css';
-import { getWeatherForecast } from '../../utils.ts';
+import {
+  WeatherForecast as WeatherForecastType,
+  getWeatherForecast,
+} from '../../utils.ts';
 import { CurrentWeatherCard, UpcomingWeatherCard } from '../WeatherCards.tsx';
 
-type WeatherCondition = {
-  text: string;
-  icon: string;
-  code: number;
-};
-
-type DailyForecast = Record<string, string | number | Day>;
-
-type Day = {
-  day: Record<string, number | WeatherCondition>;
-};
-
-type Forecast = {
-  forecastday: DailyForecast[];
-};
-
-export type CurrentWeather = Record<string, number | string | WeatherCondition>;
-type Location = Record<string, number | string>;
-
-export type WeatherData = {
-  location: Location;
-  current: CurrentWeather;
-  forecast: Forecast;
-};
-
-type WeatherForecastProps = {
+interface WeatherForecastProps {
   headline: string;
   defaultCity: string;
-};
+}
 
 export default function WeatherForecast({
   headline,
   defaultCity,
 }: WeatherForecastProps) {
   const [city, setCity] = useState('');
-  const [weatherData, setWeatherData] = useState<WeatherData>();
+  const [isLoading, setIsLoading] = useState(true);
+  const [weatherData, setWeatherData] = useState<WeatherForecastType>();
 
   useEffect(() => {
     getWeatherForecast(defaultCity).then((data) => {
-      setWeatherData(JSON.parse(data as string));
+      setWeatherData(data);
+      setIsLoading(false);
     });
   }, []);
 
   const handleFetchWeather = () => {
+    setIsLoading(true);
     getWeatherForecast(city).then((data) => {
-      setWeatherData(JSON.parse(data as string));
+      setWeatherData(data);
+      setIsLoading(false);
     });
   };
 
   return (
     <div className={weatherStyles.wrapper}>
-      <h2>{headline}</h2>
+      <h1>{headline}</h1>
       <div className={weatherStyles.form}>
         <input
           type="text"
@@ -63,23 +45,22 @@ export default function WeatherForecast({
         />
         <button onClick={handleFetchWeather}>Update Forecast</button>
       </div>
-      {weatherData?.forecast ? (
-        <>
-          <div className={weatherStyles.currentWeather}>
-            <CurrentWeatherCard weatherData={weatherData} />
-          </div>
-          <div className={weatherStyles.cardContainer}>
-            {weatherData.forecast.forecastday.map((weather: any, i) => {
-              if (i === 0) return null;
-              return (
-                <UpcomingWeatherCard key={weather.date} weatherData={weather} />
-              );
-            })}
-          </div>
-        </>
-      ) : (
-        <h2>No data found, please search another location</h2>
-      )}
+      <div className={weatherStyles.currentWeather}>
+        {!isLoading && weatherData?.forecast ? (
+          <>
+            <div>
+              <CurrentWeatherCard weatherData={weatherData} />
+            </div>
+            <div className={weatherStyles.cardContainer}>
+              <UpcomingWeatherCard weatherData={weatherData} />
+            </div>
+          </>
+        ) : isLoading ? (
+          <h2>Loading...</h2>
+        ) : (
+          <h2>No data found, please search another location</h2>
+        )}
+      </div>
     </div>
   );
 }
--- examples/getting-started/getting-started-project/getting-started-app/utils.ts
+++ examples/getting-started/getting-started-project/getting-started-app/utils.ts
@@ -1,65 +1,95 @@
 import { ICON_MAP } from './constants.ts';
 
+const FORECAST_BASE_URL = 'https://api.open-meteo.com/v1/forecast';
+const LAT_LNG_BASE_URL = 'https://geocoding-api.open-meteo.com/v1/search';
+
+interface Forecast {
+  time: string[];
+  apparent_temperature_max: number[];
+  weather_code: number[];
+}
+
+export interface ForecastData {
+  time: string;
+  apparent_temperature_max: number;
+  weather_code: number;
+}
+
+interface LocationResult {
+  latitude: number;
+  longitude: number;
+}
+
+interface LocationResponse {
+  results: LocationResult[];
+}
+
+interface ForecastResponse {
+  daily: Forecast;
+}
+
+export interface WeatherForecast {
+  city: string;
+  forecast: ForecastData[];
+}
+
+interface DailyUnits {
+  time: string;
+  apparent_temperature_max: string;
+  weather_code: string;
+}
+
+export interface ApiResponse {
+  latitude: number;
+  longitude: number;
+  generationtime_ms: number;
+  utc_offset_seconds: number;
+  timezone: string;
+  timezone_abbreviation: string;
+  elevation: number;
+  daily_units: DailyUnits;
+  daily: Forecast;
+}
+
+// codes pulled from the "WMO Weather interpretation codes" section of the open-meteo docs: https://open-meteo.com/en/docs
 export function getWeatherIcon(type: string) {
   switch (type) {
-    case 'Sunny':
+    case '0':
+    case '1':
     case 'Clear':
       return ICON_MAP.CLEAR;
 
-    case 'Partly Cloudy':
-    case 'Cloudy':
-    case 'Overcast':
+    case '2':
+    case '3':
       return ICON_MAP.CLOUDY;
 
-    case 'Mist':
-    case 'Patchy rain possible':
-    case 'Light drizzle':
-    case 'Light rain':
-    case 'Moderate rain at times':
-    case 'Moderate rain':
-    case 'Heavy rain at times':
-    case 'Heavy rain':
-    case 'Light rain shower':
-    case 'Moderate or heavy rain shower':
-    case 'Torrential rain shower':
-    case 'Patchy light rain':
-    case 'Freezing drizzle':
-    case 'Heavy freezing drizzle':
-    case 'Light freezing rain':
-    case 'Moderate or heavy freezing rain':
-    case 'Patchy rain nearby':
+    case '51':
+    case '53':
+    case '55':
+    case '56':
+    case '57':
+    case '61':
+    case '63':
+    case '65':
+    case '66':
+    case '67':
       return ICON_MAP.RAIN;
 
-    case 'Patchy snow possible':
-    case 'Light snow':
-    case 'Moderate snow':
-    case 'Heavy snow':
-    case 'Patchy light snow':
-    case 'Patchy moderate snow':
-    case 'Patchy heavy snow':
-    case 'Blizzard':
-    case 'Blowing snow':
-    case 'Light snow showers':
-    case 'Moderate or heavy snow showers':
-    case 'Patchy light snow with thunder':
-    case 'Moderate or heavy snow with thunder':
-    case 'Patchy sleet possible':
-    case 'Light sleet':
-    case 'Moderate or heavy sleet':
-    case 'Light sleet showers':
-    case 'Moderate or heavy sleet showers':
-    case 'Ice pellets':
-    case 'Light showers of ice pellets':
-    case 'Moderate or heavy showers of ice pellets':
+    case '71':
+    case '73':
+    case '75':
+    case '77':
+    case '85':
+    case '86':
       return ICON_MAP.SNOW;
 
-    case 'Thundery outbreaks possible':
-    case 'Patchy light rain with thunder':
-    case 'Moderate or heavy rain with thunder':
+    case '95':
+    case '96':
+    case '99':
       return ICON_MAP.THUNDERSTORM;
 
-    case 'Fog':
-    case 'Freezing fog':
+    case '45':
+    case '48':
       return ICON_MAP.FOG;
 
     default:
@@ -67,22 +97,36 @@ export function getWeatherIcon(type: string) {
   }
 }
 
-const apiKey = '<YOUR_API_KEY>';
+function transformResponseData(data: Forecast): ForecastData[] {
+  const { time, apparent_temperature_max, weather_code } = data;
+  const transformedData = time.map((day: string, index: number) => ({
+    time: day,
+    apparent_temperature_max: apparent_temperature_max[index],
+    weather_code: weather_code[index],
+  }));
 
-const baseApiUrl = 'https://weatherapi-com.p.rapidapi.com/forecast.json';
-const options = {
-  method: 'GET',
-  headers: {
-    'x-rapidapi-key': apiKey,
-    'x-rapidapi-host': 'weatherapi-com.p.rapidapi.com',
-  },
-};
+  return transformedData;
+}
 
-export async function getWeatherForecast(city: string) {
+export async function getWeatherForecast(
+  city: string,
+): Promise<WeatherForecast> {
   try {
-    const response = await fetch(`${baseApiUrl}?q=${city}&days=3`, options);
-    const result = await response.text();
-    return result as string;
+    const locationResponse = await fetch(
+      `${LAT_LNG_BASE_URL}?name=${city}&count=1&language=en&format=json`,
+    );
+    const { results }: LocationResponse = await locationResponse.json();
+    const { latitude, longitude } = results[0];
+
+    const forecastResponse = await fetch(
+      `${FORECAST_BASE_URL}?latitude=${latitude}&longitude=${longitude}&temperature_unit=fahrenheit&wind_speed_unit=mph&precipitation_unit=inch&forecast_days=3&daily=apparent_temperature_max,weather_code`,
+    );
+
+    const forecast: ForecastResponse = await forecastResponse.json();
+
+    const transformedForecast = transformResponseData(forecast.daily);
+
+    return { city, forecast: transformedForecast };
   } catch (error) {
     console.error(error);
   }

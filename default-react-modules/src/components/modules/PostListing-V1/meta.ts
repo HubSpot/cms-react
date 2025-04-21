5a86a18dcfc22f32d5a66fe523d56ab4ca1d4187
Jon Miller <jonmiller@hubspot.com>
2025-04-21 15:41:04 -0500

    remove examples

--- default-react-modules/src/components/modules/PostListing-V1/meta.ts
+++
@@ -1,9 +0,0 @@
-import blogIcon from './assets/blog.svg';
-import { ModuleMeta } from '../../../types/modules.js'
-
-export const meta: ModuleMeta = {
-  label: 'Post listing',
-  host_template_types: ['BLOG_LISTING', 'BLOG_POST', 'PAGE'],
-  icon: blogIcon,
-  categories: ['blog'],
-};

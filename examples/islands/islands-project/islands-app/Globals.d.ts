8b486da9da69001c7b7996d07f62769bbbe81dd5
Josh Crites <josh@getwyz.com>
2025-04-29 17:02:47 -0400

    add global type for island imports
    copied directly from global types in @hubspot/cms-components

--- examples/islands/islands-project/islands-app/Globals.d.ts
+++ examples/islands/islands-project/islands-app/Globals.d.ts
@@ -1,1 +1,12 @@
 declare module '*.module.css';
+
+// copied directly from @hubspot/cms-components
+declare module '*?island' {
+  const lazyComponent: {
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    (): Promise<{ default: React.ComponentType<any> }>;
+    moduleName: string;
+    moduleId: string;
+  };
+  export default lazyComponent;
+}
